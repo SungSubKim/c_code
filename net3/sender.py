e4a0c533c6cb9d3a5b8158898baffb91abf5904b
pro0627 <rlatjdtjq627@naver.com>
2019-11-19 16:10:51 +0900

    sender.py_fixed

--- net3/sender.py
+++ net3/sender.py
@@ -1,15 +1,22 @@
 from socket import *
-ip_rcv = input("Enter the IP address of a receiver")
-t_out = input("Enter the timeout")
-w_size = input("Enter the window size")
-    
-serverSock = socket(AF_INET,SOCK_DGRAM)
-serverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-serverSock.bind(('',10080))
-print('my ip is '+gethostbyname(getfqdn()))
-while True:
-    message, clientAddress = serverSocket.recvfrom(1024)
-    newMsg = message.decode().upper()
-    serverSocket.sendto(newMsg.encode(),ip_rcv)
-serverSock.close()
+import time
+#ip = raw_input("Enter the IP address of a receiver: ")
+ip = '192.168.0.8'
+filename = raw_input("Enter the filename: ")
+senderSock = socket(AF_INET,SOCK_DGRAM)
+senderSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+senderSock.bind(('',10080))
+senderSock.sendto('hi'.encode(),(ip,10080))
+sum_content = b''
+with open(filename,"rb") as f1:
+    while 1:
+        content = f1.read(2)
+        if not content:
+            break
+        sum_content += content
+        senderSock.sendto(content,(ip,10080))
+time.sleep(15)
+print(sum_content.decode())                   
+senderSock.close()
 print('Program ended.')
+
