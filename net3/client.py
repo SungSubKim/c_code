615426d0bf46e2718a894f891f42b9b1a081f38e
bang627 <bang627@skku.edu>
2019-11-19 06:19:36 +0000

    net3

--- net3/client.py
+++ net3/client.py
@@ -4,8 +4,3 @@ clientSocket = socket(AF_INET,SOCK_DGRAM)
 clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 msg = input("Input lowercase sentence: ")
 clientSocket.sendto(msg.encode(),(ip_snd,10080))
-
-newMsg.serverAddress=clientSocket.recvfrom(1024)
-
-print(newMsg.decode())
-clientSocket.close()    
