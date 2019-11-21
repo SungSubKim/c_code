e37387f956f2607433ef90170c0494e52b62713b
ch1z <bang627@skku.edu>
2019-11-21 20:00:18 +0900

    transsmission_established

--- net3/receiver.py
+++ net3/receiver.py
@@ -4,18 +4,18 @@ receiverSock = socket(AF_INET,SOCK_DGRAM)
 receiverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 receiverSock.bind(('',10080))
 print("receiverSock is created.")
-content = b''
-new_content,serverAddress=receiverSock.recvfrom(1024)
-print(new_content.decode())
+content_sum = b''
 while 1:
-    new_content,serverAddress=receiverSock.recvfrom(2)
-    #if not new_content:
-    #    break
-    content+=new_content
-    print(new_content.decode())
-    with open("client.py","wb") as f1:
-        f1.write(content)
-        #f1.write(new_content)
+    content,serverAddress=receiverSock.recvfrom(1400)
+    header = content[:32]
+    header2 = header[12:].decode()
+    f_name = ''
+    for i in header2:
+        if i is not '\x00':
+            f_name+=i
+    #content_sum += content[32:]
+    with open(f_name,"ab") as f1:
+        f1.write(content[32:])
 print("received!")
 
 receiverSock.close()    
--- net3/sender.py
+++ net3/sender.py
@@ -5,33 +5,35 @@ def make_header(port,seq,ack):
     header = [int(port/256), port%256, int(port/256), port%256,255,255,255,255]
     header += [int(seq/256), seq%256, int(ack/256), ack%256]
     return header #len is 8+4, 12
-def make_pkt(header, content):
-    pkt_len= len(header)+len(content)
+def make_pkt(header,f_name,content):
+    pkt_len= len(header)+20+len(content)
     header[4]=int(pkt_len/256)
     header[5]=pkt_len%256
-    pkt = bytes(header)+ content
+    print(header)
+    pkt = bytes(header)+f_name.encode()+bytes(20-len(f_name))+content
     return pkt
 #ip = raw_input("Enter the IP address of a receiver: ")
-ip = '192.168.0.8'
+ip = '192.168.0.12'
 #ip = raw_input("Enter the IP address of a receiver: ")
 timeout=10
 #ip = raw_input("Enter the IP address of a receiver: ")
 w_size=4
 port=10080
-f_name = raw_input("Enter the filename: ")
+f_name = input("Enter the filename: ")
 senderSock = socket(AF_INET,SOCK_DGRAM)
 senderSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 senderSock.bind(('',port))
 seq=0
-ack=1<<16
-senderSock.sendto(f_name.encode(),(ip,10080)) 
+ack=1<<16-1
+cnt=0
 with open(f_name,"rb") as f1:
     while True:
-        header = make_header(port,seq,ack,f_name)
-        content = f1.read(1400-len(header)) #1400-12=1388
+        header = make_header(port,seq,ack)
+        content = f1.read(1400-len(header)-20) #1400-32=1368
         if not content:
             break
-        pkt = make_pkt(header, content)
+        pkt = make_pkt(header,f_name,content)
+        print(len(pkt))
         senderSock.sendto(pkt,(ip,10080))
 senderSock.close()
 print('Program ended.')
