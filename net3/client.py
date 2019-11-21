db6e1da7e96245ce3350c30ce621e6282dbe938b
pro0627 <rlatjdtjq627@naver.com>
2019-11-21 17:59:41 +0900

    net3_sender_fixed

--- net3/client.py
+++ net3/client.py
@@ -1,6 +1,21 @@
 from socket import *
-ip_snd = input("what's serverIP")
-clientSocket = socket(AF_INET,SOCK_DGRAM)
-clientSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-msg = input("Input lowercase sentence: ")
-clientSocket.sendto(msg.encode(),(ip_snd,10080))
+
+receiverSock = socket(AF_INET,SOCK_DGRAM)
+receiverSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+receiverSock.bind(('',10080))
+print("receiverSock is created.")
+content = b''
+new_content,serverAddress=receiverSock.recvfrom(1024)
+print(new_content.decode())
+while 1:
+    new_content,serverAddress=receiverSock.recvfrom(2)
+    #if not new_content:
+    #    break
+    content+=new_content
+    print(new_content.decode())
+    with open("client.py","wb") as f1:
+        f1.write(content)
+        #f1.write(new_content)
+print("received!")
+
+receiverSock.close()    
--- net3/sender.py
+++ net3/sender.py
@@ -1,22 +1,38 @@
 from socket import *
 import time
+
+def make_header(port,seq,ack):
+    header = [int(port/256), port%256, int(port/256), port%256,255,255,255,255]
+    header += [int(seq/256), seq%256, int(ack/256), ack%256]
+    return header #len is 8+4, 12
+def make_pkt(header, content):
+    pkt_len= len(header)+len(content)
+    header[4]=int(pkt_len/256)
+    header[5]=pkt_len%256
+    pkt = bytes(header)+ content
+    return pkt
 #ip = raw_input("Enter the IP address of a receiver: ")
 ip = '192.168.0.8'
-filename = raw_input("Enter the filename: ")
+#ip = raw_input("Enter the IP address of a receiver: ")
+timeout=10
+#ip = raw_input("Enter the IP address of a receiver: ")
+w_size=4
+port=10080
+f_name = raw_input("Enter the filename: ")
 senderSock = socket(AF_INET,SOCK_DGRAM)
 senderSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-senderSock.bind(('',10080))
-senderSock.sendto('hi'.encode(),(ip,10080))
-sum_content = b''
-with open(filename,"rb") as f1:
-    while 1:
-        content = f1.read(2)
+senderSock.bind(('',port))
+seq=0
+ack=1<<16
+senderSock.sendto(f_name.encode(),(ip,10080)) 
+with open(f_name,"rb") as f1:
+    while True:
+        header = make_header(port,seq,ack,f_name)
+        content = f1.read(1400-len(header)) #1400-12=1388
         if not content:
             break
-        sum_content += content
-        senderSock.sendto(content,(ip,10080))
-time.sleep(15)
-print(sum_content.decode())                   
+        pkt = make_pkt(header, content)
+        senderSock.sendto(pkt,(ip,10080))
 senderSock.close()
 print('Program ended.')
 
